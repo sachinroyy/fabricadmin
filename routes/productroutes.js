cb9e006511f5a53b6c40ba2eb0691dde09064763
kumarsachin88 <SACHINKUMARROY561@GMAIL.COM>
2025-08-13 22:58:40 +0530

    h

--- routes/productroutes.js
+++ routes/productroutes.js
@@ -2,7 +2,7 @@ import express from "express";
 import multer from "multer";
 import path from 'path';
 import fs from 'fs';
-import Product from "../models/Product.js";
+import Product from "../models/product.js";
 import { cloudinary } from "../utils/cloudinary.js";
 
 const router = express.Router();
